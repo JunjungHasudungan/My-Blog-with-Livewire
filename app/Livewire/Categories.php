ca64d32ea81da833454cddf2d289e1fdb3131471
junjunghasudungan <junjunghasudungan43@gmail.com>
2024-06-11 10:42:15 +0700

    refactoring edit Category

--- app/Livewire/Categories.php
+++ app/Livewire/Categories.php
@@ -8,14 +8,30 @@ use Livewire\Component;
 class Categories extends Component
 {
     public  $is_create = false,
+            $is_edit = false,
             $name,
-            $created_by;
+            $created_by,
+            $category;
 
     public function render()
     {
         return view('livewire.categories', [
             'listCategory'  => Category::all(),
         ]);
+    }
+
+    public function openModalEdit()
+    {
+        return $this->is_edit = true;
+    }
+
+    public function closeModalEdit()
+    {
+        return $this->is_edit = false;
+
+        $this->reset('name');
+
+        $this->resetValidation('name');
     }
 
     public function openModalCreate()
@@ -41,10 +57,11 @@ class Categories extends Component
     public function storeKategori()
     {
         $this->validate([
-            'name'  => 'required|min:5'
+            'name'  => 'required|min:5|unique:categories,name'
         ],[
             'name.required'     => 'Nama Kategori wajib diisi..',
-            'name.min'          => 'Nama kategori minimal 5 karakter..'
+            'name.min'          => 'Nama kategori minimal 5 karakter..',
+            'name.unique'       => 'Nama kategori sudah digunakan..'
         ]);
 
         Category::create([
@@ -53,6 +70,29 @@ class Categories extends Component
         ]);
 
         $this->closeModalCreate();
+
+    }
+
+    public function editCategory($idCategory)
+    {
+        $category = Category::findOrFail($idCategory);
+
+        $this->openModalEdit();
+        
+        $this->category = $category;
+
+        $this->name = $category->name;
+    }
+
+    public function updateCategory($idCategory) 
+    {
+        $category = Category::findOrFail($idCategory);
+
+        $category->update([
+            'name'  => $this->name
+        ]);
+
+        $this->closeModalEdit();
 
     }
 
--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -1,20 +1,34 @@
 <div>
     <div class="flex flex-row justify-between">
-        <div class="">
-            <button wire:click.prevent="createKategori()"
+        <div class="flex">
+            <button
+                wire:click.prevent="createKategori"
+                type="button"
+                class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-3 gap-2 mb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                  </svg>
+                Kategori
+                </button>
+            {{-- <button wire:click.prevent="createKategori()"
                     type="button"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Tambah Kateogori
-            </button>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+
+                     Kateogori
+            </button> --}}
 
         </div>
         @if ($is_create)
             @include('livewire.pages.kategori._modalCreateKategori')
         @endif
 
-        {{-- @if ($is_edit)
+        @if ($is_edit)
             @include('livewire.pages.kategori._modalEditKategori')
-        @endif --}}
+        @endif
 
         {{-- @if ($isCreateReplyPost)
 
@@ -61,13 +75,23 @@
                         {{ $category->created_at }}
                     </td>
                     <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <button wire:click="editCategory({{ $category->id }})"
+                                type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-gray-200 rounded-lg hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-bg-gray-400 dark:bg-gray-300 dark:hover:bg-gray-400 dark:focus:ring-bg-gray-400">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                              </svg>
+                        </button>
+                        <button type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-transparent dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                        </button>
                     </td>
                 </tr>
             @empty
-                <div class="mt-2 border">
-                    <p> Tidak ada Kategori</p>
-                </div>
+                 <div class="bg-yellow-500 text-white p-3 rounded shadow-sm mb-3">
+                        Data Belum Tersedia.
+                    </div>
             @endforelse
         </tbody>
     </table>
